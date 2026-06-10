86a03d59adb6f5fb47f1d7cb22bd38bc37ceb3fb
Moin Naik <moinnaik98@gmail.com>
2026-06-10 22:29:46 +0530

    added jenkins file

--- app/backend/utility/apm-agent.js
+++ app/backend/utility/apm-agent.js
@@ -4,6 +4,6 @@ config();
 
 apm.start({
   serviceName: process.env.BACKEND_SERVICE_NAME,
-  serverUrl: `${process.env.APM_URL}:${process.env.APM_PORT}`,
+  serverUrl: process.env.APM_URL,
   environment: "production",
 });
