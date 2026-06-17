60ddbc93f244e9cbed485ecc3d2d81c03d895c1c
Moin Naik <moinnaik98@gmail.com>
2026-06-17 18:48:22 +0530

    added report

--- app/backend/index.js
+++ app/backend/index.js
@@ -38,10 +38,10 @@ connectDB();
 // Health Check API
 app.get("/health", (req, res) => {
   res.status(200).json({
+    data: "this is change",
     status: "UP",
     pod: os.hostname(),
     timestamp: new Date().toISOString(),
-    update: true
   });
 });
 
